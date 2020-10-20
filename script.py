1e08e3053b69e9a563cfbcbaefb4475dd5745305
Mathieu <mathieu.schweitzer@hotmail.fr>
2020-10-20 15:51:01 +0200

    added 'lxml' to requirements txt, script.py can now select each links (books) from each articles

--- script.py
+++ script.py
@@ -5,7 +5,14 @@ url = "http://books.toscrape.com/"
 response = requests.get(url)
 
 if response.ok:
+	links = []
 	# precise lxml or 'GuessedAtParserWarning'
 	soup = BeautifulSoup(response.text, 'lxml')
 	articles = soup.findAll('article')
-	print(len(articles))
+	for article in articles:
+		a = article.find('a')
+		# use brackets to select attributs from an href tag (like a list index)
+		link = a['href']
+		links.append('http://books.toscrape.com/' + link)
+
+	print(links)
