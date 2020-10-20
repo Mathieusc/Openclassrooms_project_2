0f3cfb802757b8e47cd7afb9abe2d778a052f359
Mathieu <mathieu.schweitzer@hotmail.fr>
2020-10-20 16:13:46 +0200

    script.py can iterate through all pages from the website

--- script.py
+++ script.py
@@ -1,18 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://books.toscrape.com/"
-response = requests.get(url)
+links = []
 
-if response.ok:
-	links = []
-	# precise lxml or 'GuessedAtParserWarning'
-	soup = BeautifulSoup(response.text, 'lxml')
-	articles = soup.findAll('article')
-	for article in articles:
-		a = article.find('a')
-		# use brackets to select attributs from an href tag (like a list index)
-		link = a['href']
-		links.append('http://books.toscrape.com/' + link)
+for i in range(51):
 
-	print(links)
+	url = "http://books.toscrape.com/catalogue/page-" + str(i) + ".html"
+	response = requests.get(url)
+
+	if response.ok:
+		# precise lxml or 'GuessedAtParserWarning'
+		soup = BeautifulSoup(response.text, 'lxml')
+		articles = soup.findAll('article')
+
+		for article in articles:
+			a = article.find('a')
+			# use brackets to select attributs from an href tag (like a list index)
+			link = a['href']
+			links.append('http://books.toscrape.com/catalogue/' + link)
+
+			print(links)
