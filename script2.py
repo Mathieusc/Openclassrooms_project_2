755b92f48658e1a120b3ad976dbc37ae143f6401
Mathieu <mathieu.schweitzer@hotmail.fr>
2020-10-21 19:02:15 +0200

    script2.py can select any information (with regular expressions)

--- script2.py
+++ script2.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = "http://books.toscrape.com/catalogue/\
 1000-places-to-see-before-you-die_1/index.html"
@@ -8,5 +9,5 @@ response = requests.get(url)
 
 if response.ok:
 	soup = BeautifulSoup(response.text, 'lxml')
-	book_information = soup.find('table', {'class': 'table table-striped'})
-	print(book_information.text)
+	book_information = soup.find('table', {'class': 'table table-striped'}).find('th', text='Product Type').find_next_sibling()
+	print(book_information)
