2b724bf9e2ab87de49e6300bacf882973d6c1f8e
Mathieu <mathieu.schweitzer@hotmail.fr>
2020-10-22 19:07:02 +0200

    working on getting every books information on script2.py

--- script.py
+++ script.py
@@ -2,44 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_books_urls():
-	""" This function will return a list of all the links of each books
-	from books.toscrape.com"""
+	""" This function will return a list containing all the urls of each books
+	from every pages of books.toscrape.com"""
 
 	links = []
-
+	# Looping through the 50 pages of the website
 	for i in range(51):
-
 		url = "http://books.toscrape.com/catalogue/page-" + str(i) + ".html"
 		response = requests.get(url)
 
 		if response.ok:
-			#print('Page: ' + str(i) + '\n')
 			# precise lxml or 'GuessedAtParserWarning'
 			soup = BeautifulSoup(response.text, 'lxml')
 			articles = soup.findAll('article')
 
 			for article in articles:
 				a = article.find('a')
-				# use brackets to select attributs from an href tag (like a list)
+				# use brackets to select attributs from an href tag
 				link = a['href']
 				links.append('http://books.toscrape.com/catalogue/' + link)
 	return links
 
 def get_books_data():
+	pass
 
 
 # print(len(links)) >>> 1000
 # so we have all the books from the website
 books_urls = get_books_urls()
-#print(books_urls)
+print(books_urls)
 
 
 """
 Note to myself:
 It might be useful to add a pause with time.sleep(seconds) with the os
 module to avoid being blocked while scraping through all the pages from a
-website, or use a proxy, or with requests.timeout parameter
-It doesn't seem to be an issue with this website tho.
+website, or use a proxy, or with requests.timeout parameter.
+It does not seem to be an issue with this website tho.
 """
 
 #-----------------------------------------------------------------------------
@@ -47,7 +46,7 @@ It doesn't seem to be an issue with this website tho.
 # I will create a CSV file later
 
 """ 
-Create the file
+Create the file (writes what is in the list [links] inside a txt file)
 with open('urls.txt', 'w') as file:
 	for link in links:
 		file.write(link + '\n')
