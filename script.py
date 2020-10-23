83896d5530d45d90c8d340eba2d4c6ca45b712d6
Mathieusc <62360534+Mathieusc@users.noreply.github.com>
2020-10-23 14:42:20 +0200

    Webscraping (#1)
    * script.py can select all articles from one page
    
    * added 'lxml' to requirements txt, script.py can now select each links (books) from each articles
    
    * script.py can iterate through all pages from the website
    
    * script.py can iterate through all pages from the website
    
    * script.py can iterate through all pages from the website
    
    * added a function for script.py, working on getting books information on another file
    
    * script2.py can select any information (with regular expressions)
    
    * working on getting every books information on script2.py
    
    * working on getting every books information on script2.py
    
    * working on getting the book's picture

---
+++ script.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_books_urls():
+	""" This function will return a list containing all the urls of each books
+	from every pages of books.toscrape.com"""
+
+	links = []
+	# Looping through the 50 pages of the website
+	for i in range(51):
+		url = "http://books.toscrape.com/catalogue/page-" + str(i) + ".html"
+		response = requests.get(url)
+
+		if response.ok:
+			# precise lxml to avoid the 'GuessedAtParserWarning' message
+			soup = BeautifulSoup(response.text, 'lxml')
+			articles = soup.findAll('article')
+
+			for article in articles:
+				a = article.find('a')
+				# use brackets to select attributs from an href tag
+				link = a['href']
+				links.append('http://books.toscrape.com/catalogue/' + link)
+	return links
+
+def get_books_data():
+	pass
+
+
+# print(len(links)) >>> 1000
+# so we have all the books from the website
+books_urls = get_books_urls()
+print(books_urls)
+
+
+"""
+Note to myself:
+It might be useful to add a pause with time.sleep(seconds) with the os
+module to avoid being blocked while scraping through all the pages from a
+website, or use a proxy, or with requests.timeout parameter.
+It does not seem to be an issue with this website tho.
+"""
+
+#-----------------------------------------------------------------------------
+# Save all the urls inside a txt file for now.
+# I will create a CSV file later
+
+""" 
+Create the file (writes what is in the list [links] inside a txt file)
+with open('urls.txt', 'w') as file:
+	for link in links:
+		file.write(link + '\n')
+
+Read the file
+with open('urls.txt', 'r') as file:
+	for row in file:
+		print(row)
+"""
---
+++ script2.py
@@ -0,0 +1,59 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+import urllib3
+
+url = "http://books.toscrape.com/catalogue/the-wedding-dress_864/index.html"
+
+response = requests.get(url)
+
+if response.ok:
+	soup = BeautifulSoup(response.text, 'lxml')
+	book_upc = soup.find('table', 
+		{'class': 'table table-striped'})\
+		.find('th', text='UPC').find_next_sibling()
+	print(book_upc.text)
+
+	book_title = soup.find('h1')
+	print(book_title.text)
+
+	book_price_including_tax = soup.find('table', 
+		{'class': 'table table-striped'})\
+		.find('th', text='Price (incl. tax)').find_next_sibling()
+	print(book_price_including_tax.text)
+
+	book_price_excluding_tax = soup.find('table', 
+		{'class': 'table table-striped'})\
+		.find('th', text='Price (excl. tax)').find_next_sibling()
+	print(book_price_excluding_tax.text)
+
+	book_availability = soup.find('p', {'class': 'instock availability'})
+	print(book_availability.text.strip())
+
+	book_description = soup.find('h2').find_next()
+	#print(book_description.text)
+
+	book_category = soup.find('li', {'class': 'active'})\
+	.find_previous_sibling()
+	print(book_category.text.strip())
+
+	#book_rating = soup.find(class_=re.compile('^star-rating\ .'))
+	#print(book_rating)
+
+	book_rating = soup.find('p', {'class': re.compile(r'^star-rating\s[A-Z]')})
+	#print(book_rating)
+
+	book_picture = soup.find('img')
+	print(book_picture)
+
+	http = urllib3.PoolManager()
+	images = soup.findAll('img')
+	for image in images:
+		print(image['src'])
+
+
+
+	
+
+
+
