763c2e2899096dd8f700129503ac1d6eab2a8a75
Mathieu <mathieu.schweitzer@hotmail.fr>
2020-10-27 18:13:35 +0100

    script2.py can write all the data from every books into a csv file

--- script2.py
+++ script2.py
@@ -1,55 +1,90 @@
 import re
 import requests
+import csv
 from bs4 import BeautifulSoup
-import urllib3
 
-url = "http://books.toscrape.com/catalogue/the-wedding-dress_864/index.html"
+with open('urls.txt', 'r') as in_file:
+	with open('books.csv', 'w', encoding='utf-8') as out_file:
+		fieldnames = ['product_page_url', 'universal_product_code', 'title',\
+'price_including_tax', 'price_excluding_tax', 'number_available',\
+'product_description', 'category', 'review_rating', 'image_url']
+		csv_writer = csv.DictWriter(out_file, fieldnames=fieldnames,\
+		quoting=csv.QUOTE_ALL)
+		csv_writer.writeheader()
 
-response = requests.get(url)
+		for lines in in_file:
+			url = lines.strip()
+			print(url)
 
-if response.ok:
-	soup = BeautifulSoup(response.text, 'lxml')
-	book_upc = soup.find('table', 
-		{'class': 'table table-striped'})\
-		.find('th', text='UPC').find_next_sibling()
-	print(book_upc.text)
+			response = requests.get(url)
 
-	book_title = soup.find('h1')
-	print(book_title.text)
+			if response.ok:
+				soup = BeautifulSoup(response.text, 'lxml')
+				book_upc = soup.find('table', 
+					{'class': 'table table-striped'})\
+					.find('th', text='UPC').find_next_sibling()
+				print(book_upc.text)
 
-	book_price_including_tax = soup.find('table', 
-		{'class': 'table table-striped'})\
-		.find('th', text='Price (incl. tax)').find_next_sibling()
-	print(book_price_including_tax.text)
+				book_title = soup.find('h1')
+				print(book_title.text)
 
-	book_price_excluding_tax = soup.find('table', 
-		{'class': 'table table-striped'})\
-		.find('th', text='Price (excl. tax)').find_next_sibling()
-	print(book_price_excluding_tax.text)
+				book_price_including_tax = soup.find('table', 
+					{'class': 'table table-striped'})\
+					.find('th', text='Price (incl. tax)').find_next_sibling()
+				print(book_price_including_tax.text)
 
-	book_availability = soup.find('p', {'class': 'instock availability'})
-	print(book_availability.text.strip())
+				book_price_excluding_tax = soup.find('table', 
+					{'class': 'table table-striped'})\
+					.find('th', text='Price (excl. tax)').find_next_sibling()
+				print(book_price_excluding_tax.text)
 
-	book_description = soup.find('h2').find_next()
-	#print(book_description.text)
+				book_availability = soup.find('p'\
+					, {'class': 'instock availability'})
+				print(book_availability.text.strip())
 
-	book_category = soup.find('li', {'class': 'active'})\
-	.find_previous_sibling()
-	print(book_category.text.strip())
+				book_description = soup.find('h2').find_next()
+				print(book_description.text)
 
-	#book_rating = soup.find(class_=re.compile('^star-rating\ .'))
-	#print(book_rating)
+				book_category = soup.find('li', {'class': 'active'})\
+				.find_previous_sibling()
+				print(book_category.text.strip())
 
-	book_rating = soup.find('p', {'class': re.compile(r'^star-rating\s[A-Z]')})
-	#print(book_rating)
+				book_rating = soup.find(class_=re.compile('^star-'))
+				if book_rating.attrs['class'][1] == 'One':
+					book_rating_converted = '1'
+					print(book_rating_converted)
+				elif book_rating.attrs['class'][1] == 'Two':
+					book_rating_converted = '2'
+					print(book_rating_converted)
+				elif book_rating.attrs['class'][1] == 'Three':
+					book_rating_converted = '3'
+					print(book_rating_converted)
+				elif book_rating.attrs['class'][1] == 'Four':
+					book_rating_converted = '4'
+					print(book_rating_converted)
+				elif book_rating.attrs['class'][1] == 'Five':
+					book_rating_converted = '5'
+					print(book_rating_converted)
 
-	book_picture = soup.find('img')
-	print(book_picture)
+				book_picture = soup.find('img')
+				print('http://books.toscrape.com/' \
+					+ book_picture.attrs['src'].replace('../', ''))
+				book_picture_url = 'http://books.toscrape.com/' \
+					+ book_picture.attrs['src'].replace('../', '')
 
-	http = urllib3.PoolManager()
-	images = soup.findAll('img')
-	for image in images:
-		print(image['src'])
+				rows = [
+		{'product_page_url': url,\
+		'universal_product_code': book_upc.text,\
+		'title': book_title.text,\
+		'price_including_tax': book_price_including_tax.text,\
+		'price_excluding_tax': book_price_excluding_tax.text,\
+		'number_available': book_availability.text.strip(),\
+		'product_description': book_description.text,\
+		'category': book_category.text.strip(),\
+		'review_rating': book_rating_converted,\
+		'image_url': book_picture_url}]
+
+				csv_writer.writerows(rows)
 
 
 
