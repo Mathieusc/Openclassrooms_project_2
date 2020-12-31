4830b728445522027f143cddf122f681101ac3c9
Mathieu Schweitzer <mathieu.schweitzer@hotmail.fr>
2020-12-31 12:05:56 +0100

    pep8 changes

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ import re
 import csv
 from bs4 import BeautifulSoup
 from scraper_functions import (
+    get_category_index_url,
     get_number_of_pages, 
     get_books_urls,
     get_books_data,
@@ -28,8 +29,10 @@ def main():
     # Using only one category for now as a test, the program needs to loop
     # through all of them in the end.
     add_a_comment = "https://books.toscrape.com/catalogue/category/books/add-a-comment_18/index.html"
+    all_categories = get_category_index_url()
+    print("All categories: \n", all_categories)
     # 1 - getting all the pages from one category if they exists.
-    nb_of_pages = get_number_of_pages(add_a_comment)
+    nb_of_pages = get_number_of_pages(all_categories[6])
     print("Pages urls:\n", nb_of_pages, "\n")
 
     # 2 - getting all the urls from all the books from all the pages of one
